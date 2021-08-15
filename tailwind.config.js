32166b4b4c40872c7a3364663f428f68fa507795
mediamakermatt <mediamakermatt@gmail.com>
2021-08-15 12:50:57 -0400

    added extended colors, tested and working

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: { // allows adding on to default instead of overriding
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'spac-teal': '#0D978A',
+        'spac-green': '#07524B',
+      }
+    },
   },
   variants: {
     extend: {},
